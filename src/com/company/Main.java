65d4678a63ae113fb56c7c32f1e8bcebdebfa5d8
danedstromkyh <dan.edstrom@student.kyh.se>
2021-09-17 09:42:05 +0200

    ver.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Testing")
-                for(int i=0; i < 25; i++);
+        System.out.println("Testing");
+                for(int i=0; i < 25; i++)
+                    System.out.println(i);
 
     }
 }
